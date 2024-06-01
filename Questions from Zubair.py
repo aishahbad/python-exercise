c04066733879835d4874996c67a7ab299064c4b9
aishahbad <aishahbadiuzzaman@gmail.com>
2024-06-01 13:15:21 +0800

    python exercise

---
+++ Questions from Zubair.py
@@ -0,0 +1,10 @@
+#1. Write a function that takes in two numbers and returns their sum.
+#num_one = 1
+#num_two = 2
+num_one = 1
+num_two = 2
+sum = num_one + num_two
+print(sum)
+print(1+2)
+name = "bedezub"
+len(name)
